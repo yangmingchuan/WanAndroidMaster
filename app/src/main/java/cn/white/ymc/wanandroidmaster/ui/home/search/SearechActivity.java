581affa45eed05b721bd10329dead8afa86f167d
yangmingchuan <18768880074@163.com>
2018-08-25 21:13:33 +0800

    1. cv 部分搜索界面 ui

--- app/src/main/java/cn/white/ymc/wanandroidmaster/ui/home/search/SearechActivity.java
+++ app/src/main/java/cn/white/ymc/wanandroidmaster/ui/home/search/SearechActivity.java
@@ -1,18 +1,38 @@
 package cn.white.ymc.wanandroidmaster.ui.home.search;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
+import com.zhy.view.flowlayout.TagFlowLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.white.ymc.wanandroidmaster.R;
 import cn.white.ymc.wanandroidmaster.base.BaseResultActivity;
 
 /**
  * 搜索界面
- *
+ * <p>
  * 2018年8月24日 15:23:08
+ *
  * @author ymc
  */
 public class SearechActivity extends BaseResultActivity {
+
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
+    @BindView(R.id.flow_search)
+    TagFlowLayout flowSearch;
+    @BindView(R.id.tv_clear)
+    TextView tvClear;
+    @BindView(R.id.rv_history)
+    RecyclerView rvHistory;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected int getLayoutId() {
@@ -20,7 +40,26 @@ public class SearechActivity extends BaseResultActivity {
     }
 
     @Override
+    protected void initToolbar() {
+        super.initToolbar();
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+    }
+
+    @Override
     protected void initData() {
 
     }
+
 }
