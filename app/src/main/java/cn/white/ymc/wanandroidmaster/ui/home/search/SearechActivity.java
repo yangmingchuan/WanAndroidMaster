70b5420ff19d5ddcbdfa463bf83fc709bf8272fc
yangmingchuan <18768880074@163.com>
2018-08-27 18:44:36 +0800

    1 添加搜索界面 和部分 menu 布局

--- app/src/main/java/cn/white/ymc/wanandroidmaster/ui/home/search/SearechActivity.java
+++ app/src/main/java/cn/white/ymc/wanandroidmaster/ui/home/search/SearechActivity.java
@@ -4,10 +4,13 @@ import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
 import com.zhy.view.flowlayout.TagFlowLayout;
 
 import java.util.ArrayList;
@@ -20,6 +23,8 @@ import cn.white.ymc.wanandroidmaster.base.BaseResultActivity;
 import cn.white.ymc.wanandroidmaster.data.bean.HotBean;
 import cn.white.ymc.wanandroidmaster.data.bean.HotKeyBean;
 import cn.white.ymc.wanandroidmaster.ui.home.search.adapter.SearechAdapter;
+import cn.white.ymc.wanandroidmaster.util.ColorUtil;
+import cn.white.ymc.wanandroidmaster.util.ConstantUtil;
 
 /**
  * 搜索界面
@@ -42,10 +47,16 @@ public class SearechActivity extends BaseResultActivity implements SearechContra
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    /**
+     * 本地保存历史记录
+     */
     private List<String> historyList;
     private SearechContract.Presenter presenter;
     private SearechAdapter adapter;
-    private List<HotBean> hotBeans;
+    /**
+     * 热搜 词语
+     */
+    private List<HotKeyBean> hotBeans;
 
     @Override
     protected int getLayoutId() {
@@ -79,18 +90,58 @@ public class SearechActivity extends BaseResultActivity implements SearechContra
         adapter.setOnItemClickListener(this);
         adapter.setOnItemChildClickListener(this);
         rvHistory.setAdapter(adapter);
-
+        presenter.getHotListResult();
+        presenter.getHistoryList(context,historyList);
         showLoading();
     }
 
     @Override
     public void getHotListOk(List<HotKeyBean> beanList) {
+        hotBeans.clear();
+        hotBeans.addAll(beanList);
+        adapter.notifyDataSetChanged();
+        initFlowLayout();
         showNormal();
+    }
+
+    /**
+     * 初始化 flowLayout
+     */
+    private void initFlowLayout() {
+        TagAdapter<HotKeyBean> tagAdapter = new TagAdapter<HotKeyBean>(hotBeans) {
+            @Override
+            public View getView(FlowLayout parent, int position, HotKeyBean searchHot) {
+                TextView text = (TextView) getLayoutInflater().inflate(R.layout.textview_item_hot, null);
+                String name = searchHot.getName();
+                text.setText(name);
+                text.setTextColor(ColorUtil.getRandomColor());
+                return text;
+            }
+        };
+        flowSearch.setAdapter(tagAdapter);
+        flowSearch.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
+            @Override
+            public boolean onTagClick(View view, int position, FlowLayout parent) {
+                String name = hotBeans.get(position).getName();
+                if (!historyList.contains(name)) {
+                    historyList.add(name);
+                    presenter.saveHistory(context, historyList);
+                }
+                return true;
+            }
+        });
     }
 
     @Override
     public void getHotListErr(String err) {
         showError(err);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     /**
